a7e23e55d3b7b69be23b0df9df94f99b4804017c
HelAige <helena.aige@gmail.com>
2023-02-03 12:06:09 +0100

    first commit

---
+++ src/components/ToDoList/index.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {ToDoForm} from '../ToDoForm';
+import { ToDo } from '../ToDo';
+
+
+
+export function ToDoList() {
+    const [toDos, setToDos] = useState<any[]>([]);
+    const addToDo = (toDo:any) => {
+        if (!toDo.text || /^\s*$/.test(toDo.text)) {
+            return
+        }
+
+        const newToDos:any = [toDo, ...toDos];
+        setToDos(newToDos);
+        console.log(newToDos);
+    }
+
+    const removeToDo = (id:any) => {
+        const removeArr = [...toDos].filter(toDo => toDo.id !== id);
+        setToDos(removeArr);
+    }
+    const updateToDo = (toDoId:any, newValue:any) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+            return
+        }
+        setToDos(prev => prev.map(item => (item.id === toDoId ? newValue : item)))
+    }
+    return (
+        <>
+            <h1>What's your plans for today?</h1>
+            <ToDoForm onSubmit={addToDo} />
+            <ToDo toDos={toDos} removeToDo={removeToDo} updateToDo={updateToDo} />
+        </>
+    )
+}
